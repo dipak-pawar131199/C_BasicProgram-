035ade8a4d1971712b5f45fed3e8e2f126230c28
dipak-pawar131199 <77726578+dipak-pawar131199@users.noreply.github.com>
2021-05-09 15:16:31 +0530

    Create Sum.c
    Calculate sum of two numbers by function

---
+++ Sum.c
@@ -0,0 +1,13 @@
+// Add two numbers using function
+#include<studio.h>
+int sum(int a,int b)
+ {
+   return a+b;
+  }
+ main()
+{
+  r=sum(10,20);
+  printf("sum of two numbers is %d ",r);
+  
+  return
+}
