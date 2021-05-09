82427e645c92f561cb9ce058b4e05bec4d047721
dipak-pawar131199 <77726578+dipak-pawar131199@users.noreply.github.com>
2021-05-09 15:19:21 +0530

    Update Sum.c

--- Sum.c
+++ Sum.c
@@ -1,13 +1,14 @@
 // Add two numbers using function
-#include<studio.h>
+#include<stdio.h>
 int sum(int a,int b)
  {
    return a+b;
   }
  main()
 {
+  int r;
   r=sum(10,20);
   printf("sum of two numbers is %d ",r);
   
-  return
+  return;
 }
